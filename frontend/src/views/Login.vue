2c5d30d858861421054928a53f81ccadcdc138dc
Francesco Boccia <73180464+FrancescoBoccia@users.noreply.github.com>
2021-04-09 03:42:31 -0800

    Update Login.vue

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -84,6 +84,14 @@ export default {
             if (!data.token) {
               this.errorMessage = "User not found";
             } else {
+            const storage = {
+                id: data.user.id,
+                firstName: data.user.firstName,
+                lastName: data.user.lastName,
+                email: data.user.email,
+                imageUrl: data.user.imageUrl,
+                deleted: false,
+              };
               localStorage.setItem("userToken", data.token);
               localStorage.setItem("userData", JSON.stringify(data.user));
               router.push({ name: "Posts" });
